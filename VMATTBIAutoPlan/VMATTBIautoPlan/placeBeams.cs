841e87d8c8421b0a194389f6d84066a0b946492a
crcrewso <6089951+crcrewso@users.noreply.github.com>
2023-03-02 15:12:51 -0600

    Added leg coll flip

--- VMATTBIAutoPlan/VMATTBIautoPlan/placeBeams.cs
+++ VMATTBIAutoPlan/VMATTBIautoPlan/placeBeams.cs
@@ -452,6 +452,12 @@ namespace VMATTBIautoPlan
                     beamName += String.Format("{0} ", count + 1);
                     //zero collimator rotations of two main fields for beams in isocenter immediately superior to matchline. Adjust the third beam such that collimator rotation is 90 degrees. Do not adjust 4th beam
                     double coll = collRot[j];
+                    // for allVMAT, if legs are present, last two isos have their collimator positions rotated 180 degrees
+                    if (allVMAT && (numVMATIsos - i )<2)
+                    {
+                        coll = (coll + 180) % 360;
+                    }
+                        
                     if ((numIsos > numVMATIsos) && (i == (numVMATIsos - 1)))
                     {
                         if (j < 2) coll = 0.0;
