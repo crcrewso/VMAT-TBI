f91576a21f7341d1b3f8a7bdb00274748b1af491
crcrewso <6089951+crcrewso@users.noreply.github.com>
2024-03-20 12:53:16 -0600

    Simplified iso naming

--- VMATTBIAutoPlan/VMATTBIautoPlan/isoNameHelper.cs
+++ VMATTBIAutoPlan/VMATTBIautoPlan/isoNameHelper.cs
@@ -11,31 +11,38 @@ namespace VMATTBIautoPlan
         public List<string> getIsoNames(int numVMATIsos, int numIsos)
         {
             List<string> isoNames = new List<string> { };
-
-            isoNames.Add("Head");
-            if (numIsos > numVMATIsos)
+            switch (numIsos)
             {
-                if (numVMATIsos == 2) isoNames.Add("Pelvis");
-                else
-                {
+                case 2:
+                    isoNames.Add("Head");
+                    isoNames.Add("Pelvis");
+                    break;
+                case 3:
+                    isoNames.Add("Head");
                     isoNames.Add("Chest");
-                    if (numVMATIsos == 3) isoNames.Add("Pelvis");
-                    else if (numVMATIsos == 4) { isoNames.Add("Abdomen"); isoNames.Add("Pelvis"); }
-                }
-                isoNames.Add("AP / PA upper legs");
-                if (numIsos == numVMATIsos + 2) isoNames.Add("AP / PA lower legs");
-            }
-            else
-            {
-                if (numVMATIsos == 2) isoNames.Add("Pelvis");
-                else
-                {
+                    isoNames.Add("Legs");
+                    break;
+                case 4:
+                    isoNames.Add("Head");
                     isoNames.Add("Chest");
-                    if (numVMATIsos == 3) isoNames.Add("Legs");
-                    else if (numVMATIsos == 4) { isoNames.Add("Pelvis"); isoNames.Add("Legs"); }
-                    else if (numVMATIsos == 5) { isoNames.Add("Pelvis"); isoNames.Add("Legs_Sup"); isoNames.Add("Legs_Inf"); }
-                    else if (numVMATIsos == 6) { isoNames.Add("Pelvis"); isoNames.Add("Legs_Sup"); isoNames.Add("Legs_Inf"); isoNames.Add("Feet"); }
-                }
+                    isoNames.Add("Pelvis");
+                    isoNames.Add("Legs");
+                    break;
+                case 5:
+                    isoNames.Add("Head");
+                    isoNames.Add("Chest");
+                    isoNames.Add("Pelvis");
+                    isoNames.Add("Legs_Sup");
+                    isoNames.Add("Legs_Inf");
+                    break;
+                case 6:
+                    isoNames.Add("Head");
+                    isoNames.Add("Chest");
+                    isoNames.Add("Pelvis");
+                    isoNames.Add("Legs_Sup");
+                    isoNames.Add("Legs_Inf");
+                    isoNames.Add("Feet");
+                    break;
             }
             return isoNames;
         }
