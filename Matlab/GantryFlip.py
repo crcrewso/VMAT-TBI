ad735cc8cc5c4cdf58e0f8ef696fb13194d856c1
crcrewso <6089951+crcrewso@users.noreply.github.com>
2024-10-08 15:16:50 -0600

    Python replacement for the matlab script, partially tested

--- Matlab/GantryFlip.py
+++ Matlab/GantryFlip.py
@@ -2,6 +2,7 @@ import pydicom
 import sys
 import time
 import os
+import copy
 from gooey import Gooey, GooeyParser
 
 
@@ -10,38 +11,47 @@ def main():
     parser = GooeyParser(description='Gantry Flip')
     parser.add_argument('InputFile', widget='FileChooser', help='Select the DICOM file you want to flip')
     args = parser.parse_args()
-    file = args.InputFile
-    filename = os.path.basename(file)
-    path = os.path.dirname(file)
-    Info = pydicom.dcmread(file)
-    RPmodified = Info
+    sourceFile = args.InputFile
+    saveFile = sourceFile.replace('.dcm', '-Reversed.dcm')
+    filename = os.path.basename(sourceFile)
+    path = os.path.dirname(sourceFile)
+    referencePlan = pydicom.dcmread(sourceFile)
+    # Create a copy of the original file
+    flippedPlan = copy.deepcopy(referencePlan)
+    # Modify the copy name to indicate it's been reversed
+    flippedPlan.RTPlanLabel = referencePlan.RTPlanLabel + "-Reversed"
+    flippedPlan.SOPInstanceUID = pydicom.uid.generate_uid()
+    # Plan name safety, if the plan name is too long it will be cut
+    if (len(flippedPlan.RTPlanLabel) > 16):
+        flippedPlan.RTPlanLabel = referencePlan.RTPlanLabel[0:11] + "-Rev"
 
-    for i in range(len(Info.BeamSequence)):
-        CPcount = len(Info.BeamSequence[i].NumberOfControlPoints)
-        CPList = list(Info.BeamSequence[i].ControlPointSequence.keys())
-        ControlPoints = Info.BeamSequence[i].ControlPointSequence
+    for i in range(len(referencePlan.BeamSequence)):
+        CPcount = len(referencePlan.BeamSequence[i].ControlPointSequence)
+        CPList = list(referencePlan.BeamSequence[i].ControlPointSequence)
+        ControlPoints = referencePlan.BeamSequence[i].ControlPointSequence
+
         for j in range(CPcount):
             if j == 0:
-                Col = ControlPoints[CPList[j]].BeamLimitingDeviceAngle
+                Col = CPList[j].BeamLimitingDeviceAngle
                 if Col > 180:
                     Col = Col - 180
                 else:
                     Col = Col + 180
-                RPmodified.BeamSequence[i].ControlPointSequence[CPList[j]].BeamLimitingDeviceAngle = Col
-            Dir = ControlPoints[CPList[j]].GantryRotationDirection
+                flippedPlan.BeamSequence[i].ControlPointSequence[j].BeamLimitingDeviceAngle = Col
+            Dir = CPList[j].GantryRotationDirection
             if Dir == 'CW':
-                RPmodified.BeamSequence[i].ControlPointSequence[CPList[j]].GantryRotationDirection = 'CC'
+                flippedPlan.BeamSequence[i].ControlPointSequence[j].GantryRotationDirection = 'CC'
             elif Dir == 'CC':
-                RPmodified.BeamSequence[i].ControlPointSequence[CPList[j]].GantryRotationDirection = 'CW'
+                flippedPlan.BeamSequence[i].ControlPointSequence[j].GantryRotationDirection = 'CW'
             elif Dir == 'NONE':
                 print('Done')
                 print(i)
             else:
                 raise ValueError('No Gantry')
-            swapAngle = ControlPoints[CPList[CPcount-j-1]].GantryAngle
-            RPmodified.BeamSequence[i].ControlPointSequence[CPList[j]].GantryAngle = swapAngle
-    RPmodified.save_as(os.path.join(path, filename + '-GantryFlipped.dcm'))
-    display_message('Gantry Flip Complete. File saved as: {}-GantryFlipped.dcm')
+            swapAngle = CPList[CPcount - j - 1].GantryAngle
+            flippedPlan.BeamSequence[i].ControlPointSequence[j].GantryAngle = swapAngle
+    flippedPlan.save_as(saveFile)
+    display_message('Gantry Flip Complete. File saved as: {}')
 
 def display_message(program_message):
     message = program_message.format('\n'.join(sys.argv[1:])).split('\n')
