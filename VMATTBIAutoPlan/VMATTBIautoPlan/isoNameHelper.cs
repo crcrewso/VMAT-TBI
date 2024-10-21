86c928a3a79e551317485286c2136aa180cf297c
crcrewso <6089951+crcrewso@users.noreply.github.com>
2024-10-21 14:32:55 -0600

    added methods to determine if iso is head-first or feet-first, as well as related logic

--- VMATTBIAutoPlan/VMATTBIautoPlan/isoNameHelper.cs
+++ VMATTBIAutoPlan/VMATTBIautoPlan/isoNameHelper.cs
@@ -1,64 +1,102 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace VMATTBIautoPlan
 {
-    class IsoNameHelper
+    public static class IsoNameHelper
     {
 
-        public List<string> GetIsoNames(int numVMATIsos)
+        static readonly string[] isoNames = new string[] { "Head", "Thorax", "Abdomen", "Pelvis", "Legs", "Legs_Sup", "Legs_Inf", "Feet" };
+
+        static readonly string[] top = isoNames.Take(4).ToArray();
+        static readonly string[] bottom = isoNames.Skip(4).ToArray();
+
+        public static bool IsTop(string isoName)
         {
-            return GetIsoNames(numVMATIsos, numVMATIsos);
+            return top.Contains(isoName);
+        }
+
+        /// <summary>
+        /// Returns the DICOM label of the orientation of the isocenter based on the isoName
+        /// </summary>
+        /// <param name="isoName"></param>
+        /// <returns></returns>
+        public static string Orientation(string isoName)
+        {
+            if (IsTop(isoName))
+                return "Head First-Supine";
+            else if ( IsBottom(isoName) )
+            {
+                return "Feet First-Supine";
+            }
+            return "Unknown";
+        }
+
+        public static bool IsValidIsoName(string isoName)
+        {
+            if( isoNames.Contains(isoName)) return true;
+            return false;
         }
 
 
 
-        public List<string> GetIsoNames(int numVMATIsos, int numIsos)
+        public static bool IsBottom(string isoName)
+        {
+            return bottom.Contains(isoName);
+        }
+
+
+
+        public static List<string> GetIsoNames(int numVMATIsos) => GetIsoNames(numVMATIsos, numVMATIsos);
+
+
+        public static List<string> GetIsoNames(int numVMATIsos, int numIsos)
         {
             List<string> isoNames = new List<string> { };
             switch (numVMATIsos)
             {
                 case 2:
-                    isoNames.Add("Head");
-                    isoNames.Add("Pelvis");
+                    isoNames.Add(isoNames[0]);
+                    isoNames.Add(isoNames[3]);
                     break;
                 case 3:
-                    isoNames.Add("Head");
-                    isoNames.Add("Thorax");
-                    isoNames.Add("Legs");
+                    isoNames.Add(isoNames[0]);
+                    isoNames.Add(isoNames[3]);
+                    isoNames.Add(isoNames[4]);
                     break;
                 case 4:
-                    isoNames.Add("Head");
-                    isoNames.Add("Thorax");
-                    isoNames.Add("Pelvis");
-                    isoNames.Add("Legs");
+                    isoNames.Add(isoNames[0]);
+                    isoNames.Add(isoNames[1]);
+                    isoNames.Add(isoNames[3]);
+                    isoNames.Add(isoNames[4]);
                     break;
                 case 5:
-                    isoNames.Add("Head");
-                    isoNames.Add("Thorax");
-                    isoNames.Add("Pelvis");
-                    isoNames.Add("Legs_Sup");
-                    isoNames.Add("Legs_Inf");
+                    isoNames.Add(isoNames[0]);
+                    isoNames.Add(isoNames[1]);
+                    isoNames.Add(isoNames[3]);
+                    isoNames.Add(isoNames[5]);
+                    isoNames.Add(isoNames[6]);
                     break;
                 case 6:
-                    isoNames.Add("Head");
-                    isoNames.Add("Thorax");
-                    isoNames.Add("Pelvis");
-                    isoNames.Add("Legs_Sup");
-                    isoNames.Add("Legs_Inf");
-                    isoNames.Add("Feet");
+                    isoNames.Add(isoNames[0]);
+                    isoNames.Add(isoNames[1]);
+                    isoNames.Add(isoNames[3]);
+                    isoNames.Add(isoNames[5]);
+                    isoNames.Add(isoNames[6]);
+                    isoNames.Add(isoNames[7]);
                     break;
                 case 7:
-                    isoNames.Add("Head");
-                    isoNames.Add("Thorax");
-                    isoNames.Add("Abdomen");
-                    isoNames.Add("Pelvis");
-                    isoNames.Add("Legs_Sup");
-                    isoNames.Add("Legs_Inf");
-                    isoNames.Add("Feet");
+                    isoNames.Add(isoNames[0]);
+                    isoNames.Add(isoNames[1]);
+                    isoNames.Add(isoNames[2]);
+                    isoNames.Add(isoNames[3]);
+                    isoNames.Add(isoNames[5]);
+                    isoNames.Add(isoNames[6]);
+                    isoNames.Add(isoNames[7]);
                     break;
                 default:
                     for (int i = 0; i < numIsos; i++)
---
+++ VMATTBIAutoPlan/VMATTBIautoPlanTests/IsoNameHelperTests.cs
@@ -0,0 +1,181 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VMATTBIautoPlan;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VMATTBIautoPlan.Tests
+{
+    [TestClass()]
+    public class IsoNameHelperTests
+    {
+        static readonly string Head = "Head";
+        static readonly string Thorax = "Thorax";
+        static readonly string Abdomen = "Abdomen";
+        static readonly string Pelvis = "Pelvis";
+        static readonly string Legs = "Legs";
+        static readonly string Legs_Sup = "Legs_Sup";
+        static readonly string Legs_Inf = "Legs_Inf";
+        static readonly string Feet = "Feet";
+
+        #region Iso Name True Tests
+        [TestMethod()]
+        public void IsTopTest_Head()
+        {
+            Assert.IsTrue(IsoNameHelper.IsTop(Head));
+        }
+
+        [TestMethod()]
+        public void IsTopTest_Thorax()
+        {
+            Assert.IsTrue(IsoNameHelper.IsTop(Thorax));
+        }
+
+        [TestMethod()]
+        public void IsTopTest_Abdomen()
+        {
+            Assert.IsTrue(IsoNameHelper.IsTop(Abdomen));
+        }
+        [TestMethod()]
+        public void IsTopTest_Pelvis()
+        {
+            Assert.IsTrue(IsoNameHelper.IsTop(Pelvis));
+        }
+
+
+
+        [TestMethod()]
+        public void IsNotBottomTest_Head()
+        {
+            Assert.IsTrue(IsoNameHelper.IsTop(Thorax));
+        }
+        [TestMethod()]
+        public void IsNotBottomTest_Thorax()
+        {
+            Assert.IsTrue(IsoNameHelper.IsTop(Thorax));
+        }
+        [TestMethod()]
+        public void IsNotBottomTest_Abdomen()
+        {
+            Assert.IsTrue(IsoNameHelper.IsTop(Abdomen));
+        }
+        [TestMethod()]
+        public void IsNotBottomTest_Pelvis()
+        {
+            Assert.IsTrue(IsoNameHelper.IsTop(Pelvis));
+        }
+
+        #endregion
+
+        #region Iso Name False Tests
+
+        [TestMethod()]
+        public void IsNotTop_Legs()
+        {
+            Assert.IsFalse(IsoNameHelper.IsTop(Legs));
+        }
+
+        [TestMethod()]
+        public void IsNotTop_Legs_Sup()
+        {
+            Assert.IsFalse(IsoNameHelper.IsTop(Legs_Sup));
+        }
+
+        [TestMethod()]
+        public void IsNotTop_Legs_Inf()
+        {
+            Assert.IsFalse(IsoNameHelper.IsTop(Legs_Inf));
+        }
+
+        [TestMethod()]
+        public void IsNotTop_Feet()
+        {
+            Assert.IsFalse(IsoNameHelper.IsTop(Feet));
+        }
+
+        [TestMethod()]
+        public void IsBottomTest_Legs()
+        {
+            Assert.IsTrue(IsoNameHelper.IsBottom(Legs));
+        }
+        [TestMethod()]
+        public void IsBottomTest_Legs_Sup()
+        {
+            Assert.IsTrue(IsoNameHelper.IsBottom(Legs_Sup));
+        }
+        [TestMethod()]
+        public void IsBottomTest_Legs_Inf()
+        {
+            Assert.IsTrue(IsoNameHelper.IsBottom(Legs_Inf));
+        }
+        [TestMethod()]
+        public void IsBottomTest_Feet()
+        {
+            Assert.IsTrue(IsoNameHelper.IsBottom(Feet));
+        }
+
+        #endregion
+
+
+        #region Head First-Supine Tests
+
+        [TestMethod()]
+        public void OrientationTest_Head()
+        {
+            Assert.AreEqual("Head First-Supine", IsoNameHelper.Orientation(Head));
+        }
+
+        [TestMethod()]
+        public void OrientationTest_Thorax()
+        {
+            Assert.AreEqual("Head First-Supine", IsoNameHelper.Orientation(Thorax));
+        }
+        [TestMethod()]
+        public void OrientationTest_Abdomen()
+        {
+            Assert.AreEqual("Head First-Supine", IsoNameHelper.Orientation(Abdomen));
+        }
+        [TestMethod()]
+        public void OrientationTest_Pelvis()
+        {
+            Assert.AreEqual("Head First-Supine", IsoNameHelper.Orientation(Pelvis));
+        }
+
+        #endregion
+
+        #region Feet First-Supine Tests
+        [TestMethod()]
+        public void OrientationTest_Legs()
+        {
+            Assert.AreEqual("Feet First-Supine", IsoNameHelper.Orientation(Legs));
+        }
+        [TestMethod()]
+        public void OrientationTest_Legs_Sup()
+        {
+            Assert.AreEqual("Feet First-Supine", IsoNameHelper.Orientation(Legs_Sup));
+        }
+        [TestMethod()]
+        public void OrientationTest_Legs_Inf()
+        {
+            Assert.AreEqual("Feet First-Supine", IsoNameHelper.Orientation(Legs_Inf));
+        }
+        [TestMethod()]
+        public void OrientationTest_Feet()
+        {
+            Assert.AreEqual("Feet First-Supine", IsoNameHelper.Orientation(Feet));
+        }
+
+
+        #endregion
+
+
+        [TestMethod()]
+        public void OrientationTest_Invalid()
+        {
+            Assert.AreEqual("Unknown", IsoNameHelper.Orientation("Invalid"));
+        }
+
+    }
+}
